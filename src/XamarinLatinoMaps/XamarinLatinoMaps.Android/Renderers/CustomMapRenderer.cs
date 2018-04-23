e68d11d16cc91f6b873427e6ca2c53bffafafd68
Carlos Campos <ccampos@xinobistudio.com>
2018-04-23 01:17:28 -0600

    Custom Pin renderer implementation (#5)
    * Added iOS renderer and Custom Map control
    
    * Added Android custom renderer
    
    * Added UWP custom renderer

---
+++ src/XamarinLatinoMaps/XamarinLatinoMaps.Android/Renderers/CustomMapRenderer.cs
@@ -0,0 +1,113 @@
+﻿using Android.Content;
+using Android.Gms.Maps;
+using Android.Gms.Maps.Model;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+using Xamarin.Forms.Maps.Android;
+using Xamarin.Forms.Platform.Android;
+using XamarinLatinoMaps.Droid.Renderers;
+using XamarinLatinoMaps.Framework.Renderers;
+
+[assembly: ExportRenderer(typeof(CustomMap), typeof(CustomMapRenderer))]
+namespace XamarinLatinoMaps.Droid.Renderers
+{
+    public class CustomMapRenderer : MapRenderer, GoogleMap.IInfoWindowAdapter
+    {
+        private List<Pin> pins;
+        public CustomMapRenderer(Context context) : base(context)
+        {
+            
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Map> e)
+        {
+            base.OnElementChanged(e);
+            if(e.OldElement != null)
+            {
+                NativeMap.InfoWindowClick -= OnInfoWindowClick;
+            }
+
+            if (e.NewElement != null)
+            {
+                var formsMap = (CustomMap)e.NewElement;
+                pins = formsMap.Pins.ToList();
+                Control.GetMapAsync(this);
+            }
+        }
+
+        protected override void OnMapReady(GoogleMap map)
+        {
+            base.OnMapReady(map);
+
+            NativeMap.InfoWindowClick += OnInfoWindowClick;
+            NativeMap.SetInfoWindowAdapter(this);
+        }
+
+        protected override MarkerOptions CreateMarker(Pin pin)
+        {
+            var marker = new MarkerOptions();
+            marker.SetPosition(new LatLng(pin.Position.Latitude, pin.Position.Longitude));
+            marker.SetTitle(pin.Label);
+            marker.SetSnippet(pin.Address);
+            marker.SetIcon(BitmapDescriptorFactory.FromResource(Resource.Drawable.ic_coffee_bean));
+            return marker;
+        }
+
+        void OnInfoWindowClick(object sender, GoogleMap.InfoWindowClickEventArgs e)
+        {
+            var customPin = GetCustomPin(e.Marker);
+            if (customPin == null)
+            {
+                throw new Exception("Custom pin not found");
+            }
+
+            // Do custom stuff here
+        }
+
+        private Pin GetCustomPin(Marker marker)
+        {
+            return pins.FirstOrDefault(x => x.Position.Latitude == marker.Position.Latitude && x.Position.Longitude == marker.Position.Longitude);
+        }
+
+        public Android.Views.View GetInfoContents(Marker marker)
+        {
+            var inflater = Android.App.Application.Context.GetSystemService(Context.LayoutInflaterService) as Android.Views.LayoutInflater;
+            if (inflater != null)
+            {
+                Android.Views.View view;
+
+                var customPin = GetCustomPin(marker);
+                if (customPin == null)
+                {
+                    throw new Exception("Custom pin not found");
+                }
+
+                view = inflater.Inflate(Resource.Layout.MapInfoWindow, null);
+
+                var infoTitle = view.FindViewById<TextView>(Resource.Id.InfoWindowTitle);
+                var infoSubtitle = view.FindViewById<TextView>(Resource.Id.InfoWindowSubtitle);
+
+                if (infoTitle != null)
+                {
+                    infoTitle.Text = marker.Title;
+                }
+                if (infoSubtitle != null)
+                {
+                    infoSubtitle.Text = marker.Snippet;
+                }
+
+                return view;
+            }
+            return null;
+        }
+
+        public Android.Views.View GetInfoWindow(Marker marker)
+        {
+            return null;
+        }
+    }
+}
---
+++ src/XamarinLatinoMaps/XamarinLatinoMaps/Framework/Renderers/CustomMap.cs
@@ -0,0 +1,9 @@
+﻿using Xamarin.Forms.Maps;
+
+namespace XamarinLatinoMaps.Framework.Renderers
+{
+    public class CustomMap : Map
+    {
+
+    }
+}
